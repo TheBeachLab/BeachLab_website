ae6a0d560e9d8d312a7e3075dad351775bd39145
Fran <hola@beachlab.org>
2021-01-13 23:15:45 +0100

    logo keypress

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
-const scene = new THREE.Scene();
+function presionada(event) {
+	if (event.keyCode == 68) { //d
+		image = document.getElementById('logo');
+		image.src = 'img/wd.svg';
+	}
+}
 
-const camera = new THREE.PerspectiveCamera(45,
-	window.innerWidth / window.innerHeight,0.1,1000);
-camera.position.set(0,0,100);
-camera.lookAt(0,0,0);
+function soltada(event) {
+	if (event.keyCode == 68) { //d
+		image = document.getElementById('logo');
+		image.src = 'img/bl.svg';
+	}
+}
 
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-//create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-
-scene.add( line );
-renderer.render( scene, camera );
+document.onkeydown = presionada;
+document.onkeyup = soltada;
