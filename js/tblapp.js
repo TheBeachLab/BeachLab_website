c3ce36db9c7033cbd150506b29ed274c63fae198
Fran <hola@beachlab.org>
2021-04-07 14:18:57 -0700

    flex layout

--- js/tblapp.js
+++ js/tblapp.js
@@ -1,10 +1,15 @@
 // JS for tbl page
 // by Fran Sanchez
 
+// debug
+console.log(document.documentElement.scrollHeight);
+console.log(window.innerHeight);
+console.log(window.scrollY);
+
 // scroll function
 window.onscroll = function () {
     // calculate global scroll percent	
-    var scrollpercent = Math.round(100 * window.scrollY / (document.body.offsetHeight - window.innerHeight));
+    var scrollpercent = Math.round(100 * window.scrollY / (document.documentElement.scrollHeight - window.innerHeight));
     // apply scroll percent to top progress var
     document.querySelector("progress").value = scrollpercent
 };
